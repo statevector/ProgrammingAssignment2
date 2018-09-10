52816be5cbc98e314b741945e6234e3f87b5b358
statevector <cwillis@cern.ch>
2018-09-09 18:11:14 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The function makeCacheMatrix() creates an R object that stores a 
+## matrix and its inverse. This is accomplished with the setters set() 
+## and setinv() and the getters get() and getinv(). Crucially, 
+## makeCacheMatrix() returns a list of its functions to the parent 
+## environment, which allows for direct access of its internal state.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+        setinv	= setinv,
+        getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## The function cachesolve() computes the inverse of the matrix stored 
+## in the function makeCashMatrix(). If the inverse has already been 
+## calculated, then the cached value is returned and no computation of 
+## the inverse is performed.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+            inv <- x$getinv()
+            if(!is.null(inv)) {
+                    message("getting cached data")
+                    return(inv)
+            }
+            data <- x$get()
+            inv <- solve(data, ...)
+            x$setinv(inv)
+            inv		
 }
